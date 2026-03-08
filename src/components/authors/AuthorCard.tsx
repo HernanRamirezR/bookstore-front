175119837f11a74b1330dd5ca25fd431a4d97919
Hernan Ramirez R <h.ramirezr@uniandes.edu.co>
2026-03-07 21:36:16 -0500

    delete working

--- src/components/authors/AuthorCard.tsx
+++ src/components/authors/AuthorCard.tsx
@@ -1,15 +1,27 @@
 import { Author } from "@/types/Author"
+import { useAuthors } from "../context/AuthorsContext"
 
 interface Props {
   author: Author
 }
 
 export default function AuthorCard({ author }: Props) {
+
+  const { deleteAuthor } = useAuthors()
+  
   return (
-    <div className="max-w-xs bg-white shadow-md rounded-lg p-4 text-center">
-        <img src={author.image} alt={`Foto de ${author.name}`} className="w-30 h-30 mx-auto rounded-full mb-4 object-cover bg-gray-200 text-black text-sm"/>
+    <div className="max-w-xs bg-white shadow-md rounded-lg p-4 flex flex-col text-center h-full">
+        <img src={author.image || "https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png"} alt={`Foto de ${author.name}`} className="w-30 h-30 mx-auto rounded-full mb-4 object-cover bg-gray-200 text-black text-sm"/>
         <h2 className="text-xl font-semibold text-black">{author.name}</h2>
         <p className="text-black">{author.description}</p>
+        <div className="grid grid-cols-2 gap-2 mt-auto">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-3 rounded transition">
+            Edit
+          </button>
+          <button onClick={() => deleteAuthor(author.id)} className="bg-red-500 hover:bg-red-600 text-white py-1 px-3 rounded transition">
+            Delete
+          </button>
+        </div>
     </div>
   )
 }
