4636837718b959c435a4c8bccbc50a8e75131b97
Hernan Ramirez <h.ramirezr@uniandes.edu.co>
2026-03-09 10:05:25 -0500

    Filter done

--- src/components/authors/AuthorsList.tsx
+++ src/components/authors/AuthorsList.tsx
@@ -2,18 +2,40 @@
 
 import { useAuthors } from "@/components/context/AuthorsContext"
 import AuthorCard from "./AuthorCard"
+import { useState } from "react";
+
 
 export default function AuthorsList() {
 
     const { authors } = useAuthors()
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const normalizedSearch = searchTerm.toLowerCase()
+    
 
     return (
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-            {authors.map( (author) => (
-                <AuthorCard key={author.id} author={author} />
-                ))}
+            <div className="space-y-6">
+                <input
+                    type="text"
+                    placeholder="Search author by name..."
+                    value={searchTerm}  
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    className="w-full p-2 border rounded-md"
+                />
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                    {authors.filter(author => author.name.toLowerCase().includes(normalizedSearch)).map((author) => (
+                            <AuthorCard key={author.id} author={author} />
+                        ))}
+                </div>
+
+                {authors.filter(author => author.name.toLowerCase().includes(normalizedSearch)).length === 0 && (
+                    <p className="text-gray-500">
+                        No se encontraron coincidencias.
+                    </p>
+                )}
+
             </div>
-        
+            
     )
 
 }
